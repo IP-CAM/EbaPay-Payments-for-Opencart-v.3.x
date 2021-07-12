fb06b3270152323f2ca3d5399488a8e3459326bf
mohamad <mmohhamadd@gmail.com>
2021-07-12 20:34:07 +0430

    fix language

--- admin/language/en-gb/extension/payment/ebapay.php
+++ admin/language/en-gb/extension/payment/ebapay.php
@@ -10,7 +10,7 @@ $_['text_extension'] = 'افزونه ها';
 $_['text_ebapay'] = '<a href="https://ebapay.ir" target="_blank"><img src="https://ebapay.ir/images/ebapaylogo.png" alt="ebapay" title="eabpay" style="border:1px solid #eee;" /></a>';
 
 // Entry
-$_['entry_api'] = 'کلید API دریافتی';
+$_['entry_api'] = 'شناسه درگاه پرداخت';
 $_['entry_send'] = 'آدرس ارسال داده ها';
 $_['entry_verify'] = 'آدرس تایید تراکنش';
 $_['entry_gateway'] = 'آدرس انتقال به صفحه پرداخت';
